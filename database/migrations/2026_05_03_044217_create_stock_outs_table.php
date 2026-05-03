8c557f567d78406657bca76998971ac0da7e96b1
Francis <brakay1136@gmail.com>
2026-05-02 21:22:40 -0800

    database tables and migrations created

---
+++ database/migrations/2026_05_03_044217_create_stock_outs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stock_outs', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('product_id')->constrained();
+
+            $table->integer('quantity');
+            $table->decimal('selling_price', 10, 20)->nullable();
+
+            $table->date('date')->nullable();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stock_outs');
+    }
+};
